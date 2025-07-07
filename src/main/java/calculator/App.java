274ea4727571d29135f5b7995dc6c82bca36f1fd
dmlal <opkl8781@naver.com>
2025-07-07 14:54:20 +0900

    [feat] 숫자 검증  #1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,11 +6,32 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("첫 번째 숫자를 입력하세요: ");
-        int firstNum = sc.nextInt();
+        int firstNum = 0;
+        do {
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            while (!sc.hasNextInt()){
+                System.out.println("숫자를 입력해주세요.");
+                sc.next();
+            }
+            firstNum = sc.nextInt();
+            if (firstNum < 0) {
+                System.out.println("0 이상의 숫자를 입력해주세요.");
+            }
+        } while (firstNum < 0);
 
-        System.out.println("두 번째 숫자를 입력하세요: ");
-        int secondNum = sc.nextInt();
+        int secondNum = 0;
+        do {
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            while (!sc.hasNextInt()){
+                System.out.println("숫자를 입력해주세요.");
+                sc.next();
+            }
+            secondNum = sc.nextInt();
+            if (secondNum < 0) {
+                System.out.println("0 이상의 숫자를 입력해주세요.");
+            }
+        } while (secondNum < 0);
+
 
         System.out.println("사칙연산 기호를 입력하세요: ");
         char operator = sc.next().charAt(0);
